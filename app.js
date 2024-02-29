ba321e3daa6e69d7bc0a10e81b434ec8d1035c3a
Alex <mr.alex4799@gmail.com>
2024-03-01 00:06:28 +0630

    portfolio v-3

---
+++ app.js
@@ -0,0 +1,24 @@
+var typed = new Typed('.auto-type', {
+    strings: ['Htet Arkar Lin, a Website Developer from Yangon.'],
+    typeSpeed: 100,
+  });
+
+function setFontColor(color){
+    let body=document.getElementsByTagName('body')[0];
+    body.className='';
+    body.classList.add(color);
+}
+
+let menuBar=document.getElementById('menu_bar');
+let xMark=document.getElementById('x_mark');
+
+menuBar.addEventListener('click',function(){
+    let slideMenu = document.getElementById('slide-menu');
+    slideMenu.style.width='80vw';
+
+})
+xMark.addEventListener('click',function(){
+    let slideMenu = document.getElementById('slide-menu');
+    slideMenu.style.width='0vw';
+
+})
